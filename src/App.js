1b2223d14537141cc220c775cce78bb47b07458f
hemaddlawar <hema.200219627512@gmail.com>
2023-06-01 18:56:11 +0300

    Add new feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
-import { TodoWrapper } from './components/TodoWrapper';
+import { TodoWrapper } from "./components/TodoWrapper";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
     <div className="App">
-      <TodoWrapper  />
+      <TodoWrapper />
     </div>
   );
 }
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -3,7 +3,15 @@ import { Todo } from "./Todo";
 import { TodoForm } from "./TodoForm";
 import { v4 as uuidv4 } from "uuid";
 import { EditTodoForm } from "./EditTodoForm";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faGithub,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 export const TodoWrapper = () => {
   const [todos, setTodos] = useState([]);
 
@@ -60,7 +68,23 @@ export const TodoWrapper = () => {
           )
         )}
       </div>
-      <div className="dev"> developed by : 'Hema DO'</div>
+      <div className="dev">developed by : 'Hema DO'</div>
+
+      <div>
+        <a href="https://www.facebook.com/hema.dlawar.777" className="fa">
+          <FontAwesomeIcon icon={faFacebook} className="ff" />
+        </a>
+
+        <a href="https://github.com/hemadlawar" className="fa">
+          <FontAwesomeIcon icon={faGithub} className="ff" />
+        </a>
+        <a href="https://www.instagram.com/hema__dlawar/" className="fa">
+          <FontAwesomeIcon icon={faInstagram} className="ff" />
+        </a>
+        <a href="https://twitter.com/dor_hema" className="fa">
+          <FontAwesomeIcon icon={faTwitter} className="ff" />
+        </a>
+      </div>
     </>
   );
 };
